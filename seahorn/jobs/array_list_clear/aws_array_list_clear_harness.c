915a608d0735f6e01717ff95357f0920f59dd12b
Yakir Vizel <yakir.vizel@gmail.com>
2020-07-20 11:30:11 +0300

    A few new examples

---
+++ seahorn/jobs/array_list_clear/aws_array_list_clear_harness.c
@@ -0,0 +1,38 @@
+/*
+ * 
+ */
+
+#include <seahorn/seahorn.h>
+#include <aws/common/array_list.h>
+#include <array_list_helper.h>
+
+/**
+ * Runtime: 5s
+ */
+int main() {
+    /* data structure */
+    struct aws_array_list list;
+
+    initialize_array_list(&list);
+
+    /* assumptions */
+    assume(aws_array_list_is_bounded(&list, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE));
+    ensure_array_list_has_allocated_data_member(&list);
+    assume(aws_array_list_is_valid(&list));
+
+    /* save current state of the data structure */
+    struct aws_array_list old = list;
+
+    /* perform operation under verification */
+    aws_array_list_clear(&list);
+
+    /* assertions */
+    sassert(aws_array_list_is_valid(&list));
+    sassert(list.length == 0);
+    sassert(list.alloc == old.alloc);
+    sassert(list.current_size == old.current_size);
+    sassert(list.item_size == old.item_size);
+    sassert(list.data == old.data);
+
+    return 0;
+}
