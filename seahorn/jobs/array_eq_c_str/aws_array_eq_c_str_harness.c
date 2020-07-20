17d8d027edaf7b515694b335414265b755de6b90
Yakir Vizel <yakir.vizel@gmail.com>
2020-07-20 11:40:14 +0300

    One more example

---
+++ seahorn/jobs/array_eq_c_str/aws_array_eq_c_str_harness.c
@@ -0,0 +1,46 @@
+/*
+ * 
+ */
+
+#include <seahorn/seahorn.h>
+#include <aws/common/byte_buf.h>
+#include <proof_allocators.h>
+#include <utils.h>
+
+int main() {
+    /* assumptions */
+    size_t array_len = nd_size_t();
+    assume(array_len <= MAX_BUFFER_SIZE);
+    void *array = bounded_malloc(MAX_BUFFER_SIZE);
+    const char *c_str = nd_bool() ? NULL : ensure_c_str_is_allocated(MAX_BUFFER_SIZE);
+
+    /* save current state of the parameters */
+    struct store_byte_from_buffer old_byte_from_array;
+    save_byte_from_array((uint8_t *)array, array_len, &old_byte_from_array);
+    size_t str_len = (c_str) ? sea_strlen(c_str) : 0;
+    struct store_byte_from_buffer old_byte_from_str;
+    save_byte_from_array((uint8_t *)c_str, str_len, &old_byte_from_str);
+
+    /* pre-conditions */
+    assume(array || (array_len == 0));
+    assume(c_str);
+
+    /* operation under verification */
+    if (aws_array_eq_c_str(array, array_len, c_str)) {
+        /* asserts equivalence */
+        sassert(array_len == str_len);
+        if (array_len > 0) {
+            assert_bytes_match((uint8_t *)array, (uint8_t *)c_str, array_len);
+        }
+    }
+
+    /* asserts both parameters remain unchanged */
+    if (array_len > 0) {
+        assert_byte_from_buffer_matches((uint8_t *)array, &old_byte_from_array);
+    }
+    if (str_len > 0) {
+        assert_byte_from_buffer_matches((uint8_t *)c_str, &old_byte_from_str);
+    }
+
+    return 0;
+}
