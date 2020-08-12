2a9179047a6979282d76da64b991725fcd4c7c90
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-08-11 21:48:46 -0400

    fix(bcmp): implementation with static unrolling
    Easier to get default unrolling bound based on MAX_BUFFER_SIZE macro

--- seahorn/lib/bcmp.c
+++ seahorn/lib/bcmp.c
@@ -2,6 +2,10 @@
 #include <stdint.h>
 #include <strings.h>
 
+#ifndef MAX_BUFFER_SIZE
+#error "MAX_BUFFER_SIZE must be defined, try 16"
+#define MAX_BUFFER_SIZE 16
+#endif
 /**
   The bcmp() function compares the two byte sequences s1 and s2 of
   length n each.  If they are equal, and in particular if n is zero,
@@ -15,9 +19,20 @@ int bcmp(const void *s1, const void *s2, size_t n) {
   p1 = s1;
   p2 = s2;
 
-  if (p1 == p2) return 0;
-  if (p1 == NULL || p2 == NULL) return 1;
-  for (i = 0; i < n; i++) {
+  if (p1 == p2)
+    return 0;
+  if (p1 == NULL || p2 == NULL)
+    return 1;
+  /* pre-unroll the loop for MAX_BUFFER_SIZE */
+  for (i = 0; i < MAX_BUFFER_SIZE; i++) {
+    if (i < n) {
+      if (p1[i] != p2[i]) {
+        return 1;
+      }
+    }
+  }
+  /* unroll the rest, if any */
+  for (i = MAX_BUFFER_SIZE; i < n; i++) {
     if (p1[i] != p2[i])
       return 1;
   }
