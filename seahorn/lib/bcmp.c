fb283df800a7adb0267457181cd516e43184ed56
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-08-13 21:36:43 -0400

    feat(lib): implement memcmp
    On Linux, `memcmp()` is optimized to `bcmp()`, on Mac it is not.
    Provide both implementations in the library

--- seahorn/lib/bcmp.c
+++ seahorn/lib/bcmp.c
@@ -11,8 +11,8 @@
   length n each.  If they are equal, and in particular if n is zero,
   bcmp() returns 0.  Otherwise, it returns a nonzero result.
 */
-__attribute__((always_inline))
-int bcmp(const void *s1, const void *s2, size_t n) {
+__attribute__((always_inline)) int bcmp(const void *s1, const void *s2,
+                                        size_t n) {
   size_t i;
 
   const uint8_t *p1;
@@ -36,6 +36,35 @@ int bcmp(const void *s1, const void *s2, size_t n) {
   for (i = MAX_BUFFER_SIZE; i < n; i++) {
     if (p1[i] != p2[i])
       return 1;
+  }
+
+  return 0;
+}
+
+__attribute__((always_inline)) int memcmp(const void *s1, const void *s2,
+                                          size_t n) {
+  size_t i;
+
+  const uint8_t *p1;
+  const uint8_t *p2;
+  p1 = s1;
+  p2 = s2;
+
+  if (p1 == p2)
+    return 0;
+
+  /* pre-unroll the loop for MAX_BUFFER_SIZE */
+  for (i = 0; i < MAX_BUFFER_SIZE; i++) {
+    if (i < n) {
+      if (p1[i] != p2[i]) {
+        return p1[i] < p2[i] ? -1 : 1;
+      }
+    }
+  }
+  /* unroll the rest, if any */
+  for (i = MAX_BUFFER_SIZE; i < n; i++) {
+    if (p1[i] != p2[i])
+      return p1[i] < p2[i] ? -1 : 1;
   }
 
   return 0;
