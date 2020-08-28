44bc348e0e98776c66a0bb34da3b840f19a28a07
Yakir Vizel <yakir.vizel@gmail.com>
2020-08-28 17:17:55 +0300

    All array_list but sort

---
+++ seahorn/jobs/array_list_init_dynamic/aws_array_list_init_dynamic_harness.c
@@ -0,0 +1,41 @@
+/*
+ *
+ */
+
+#include <seahorn/seahorn.h>
+#include <aws/common/array_list.h>
+#include <array_list_helper.h>
+#include <utils.h>
+#include <proof_allocators.h>
+
+/**
+ * Runtime: 6s
+ */
+int main() {
+    /* data structure */
+    struct aws_array_list list;
+    initialize_array_list(&list);
+
+    /* parameters */
+    struct aws_allocator *allocator = _allocator(); /* Precondition: allocator is non-null */
+
+    size_t item_size = nd_size_t();
+    assume(item_size > 0 && item_size <= MAX_ITEM_SIZE);
+    size_t initial_item_allocation = nd_size_t();
+    assume(initial_item_allocation <= MAX_INITIAL_ITEM_ALLOCATION);
+
+    /* perform operation under verification */
+    if (aws_array_list_init_dynamic(&list, allocator, initial_item_allocation, item_size) == AWS_OP_SUCCESS) {
+        /* assertions */
+        sassert(aws_array_list_is_valid(&list));
+        sassert(list.alloc == allocator);
+        sassert(list.item_size == item_size);
+        sassert(list.length == 0);
+        sassert(list.current_size == item_size * initial_item_allocation);
+    } else {
+        /*assertions */
+        sassert(AWS_IS_ZEROED(list));
+    }
+
+    return 0;
+}
---
+++ seahorn/jobs/array_list_length/aws_array_list_length_harness.c
@@ -0,0 +1,36 @@
+/*
+ *
+ */
+
+#include <seahorn/seahorn.h>
+#include <aws/common/array_list.h>
+#include <array_list_helper.h>
+#include <utils.h>
+#include <proof_allocators.h>
+
+/**
+ * Runtime: 7s
+ */
+int main() {
+    /* data structure */
+    struct aws_array_list list;
+    initialize_array_list(&list);
+
+    /* assumptions */
+    assume(aws_array_list_is_bounded(&list, MAX_INITIAL_ITEM_ALLOCATION, MAX_ITEM_SIZE));
+    assume(aws_array_list_is_valid(&list));
+
+    /* save current state of the data structure */
+    struct aws_array_list old = list;
+    struct store_byte_from_buffer old_byte;
+    save_byte_from_array((uint8_t *)list.data, list.current_size, &old_byte);
+
+    /* perform operation under verification */
+    size_t len = aws_array_list_length(&list);
+
+    /* assertions */
+    sassert(aws_array_list_is_valid(&list));
+    assert_array_list_equivalence(&list, &old, &old_byte);
+
+    return 0;
+}
