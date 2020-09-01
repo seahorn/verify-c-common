4b9b83a05609351b5bc882510aff927b205618b7
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-09-01 16:03:33 -0400

    fix(lib): interpretation of aws_raise_error
    The default behavior is to do nothing and return.
    I missed that, thinking that default behavior is abort.
    
    Re-enabling to cover more error handling code.

--- seahorn/lib/utils.c
+++ seahorn/lib/utils.c
@@ -146,8 +146,10 @@ const char *ensure_c_str_is_allocated(size_t max_size) {
 void aws_raise_error_private(int err) {
 #ifdef AWS_ERROR_IS_VERIFIER_ERROR
   __VERIFIER_error();
+#elif AWS_ERROR_IS_EXIT
+  exit(1)
 #else
-  exit(1);
+  return;
 #endif
 }
 #endif
