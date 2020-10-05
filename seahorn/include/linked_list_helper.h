fe0e9d4f60ac1d0b963fa7ca6b97541a6f983782
Daniel Zhou <danzhou1810@gmail.com>
2020-10-05 01:07:52 -0400

    feat(jobs): add jobs for linkedlist rbegin/rend
    added rend job; rewrote linst nd init function
    
    refactor linst allocate nd

--- seahorn/include/linked_list_helper.h
+++ seahorn/include/linked_list_helper.h
@@ -5,6 +5,58 @@
 #include <stddef.h>
 
 void ensure_linked_list_is_allocated(struct aws_linked_list *const list, size_t max_length, size_t *length);
-bool sea_aws_linked_list_is_valid(const struct aws_linked_list *list, size_t length);
-void attach_nodeA_to_nodeB(struct aws_linked_list_node *nodeA, struct aws_linked_list_node *nodeB, bool directlyAttached);
 
+bool sea_aws_linked_list_is_valid(const struct aws_linked_list *list, size_t length);
+
+/*
+    Half of aws_linked_list_insert_after: sets up double linkage of
+    *to_attach* after *after*, but does not link old after->next after to_attach
+*/
+void aws_linked_list_attach_after(struct aws_linked_list_node *after,
+                                  struct aws_linked_list_node *to_attach,
+                                  bool directly_attached);
+
+/*
+    For functions that only access the front node (head.next)
+    Non-deterministically initialize aws_linked_list of the forms:
+    If size == 0, then
+    HEAD <--> TAIL
+    If size == 1, then
+    HEAD <--> FRONT <--> TAIL
+    If size == 2, then
+    HEAD <--> FRONT <--> BACK <--> TAIL
+    If size > 2, then
+    HEAD <--> FRONT <--> FRONT_NEXT --> nd_ptr ... nd_ptr <-- TAIL
+*/
+void sea_nd_init_aws_linked_list_from_front(struct aws_linked_list *list,
+                                            size_t *length);
+
+/*
+    For functions that only access the back node (tail.prev)
+    Non-deterministically initialize aws_linked_list of the forms:
+    If size == 0, then
+    HEAD <--> TAIL
+    If size == 1, then
+    HEAD <--> BACK <--> TAIL
+    If size == 2, then
+    HEAD <--> FRONT <--> BACK <--> TAIL
+    If size > 2, then
+    HEAD --> nd_ptr ... nd_ptr <-- BACK_PREV <--> BACK <--> TAIL
+*/
+void sea_nd_init_aws_linked_list_from_back(struct aws_linked_list *list,
+                                            size_t *length);
+
+/*
+    For functions that only access head and tail
+    Non-deterministically initialize aws_linked_list of the forms:
+    If size == 0, then
+    HEAD <--> TAIL
+    If size == 1, then
+    HEAD <--> FRONT <--> TAIL
+    If size == 2, then
+    HEAD <--> FRONT <--> BACK <--> TAIL
+    If size > 2, then
+    HEAD <--> FRONT --> nd_ptr ... nd_ptr <-- BACK <--> TAIL
+*/
+void sea_nd_init_aws_linked_list(struct aws_linked_list *list,
+                                            size_t *length);
