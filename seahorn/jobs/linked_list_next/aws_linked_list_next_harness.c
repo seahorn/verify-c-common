4ff16a6729e3ba0e85a496680c2ffd220765a08f
Emmy Ni <emmyni220@gmail.com>
2020-10-06 20:57:51 -0400

    Add linked_list reset and next

--- seahorn/jobs/linked_list_next/aws_linked_list_next_harness.c
+++ seahorn/jobs/linked_list_next/aws_linked_list_next_harness.c
@@ -2,19 +2,9 @@
 #include <aws/common/linked_list.h>
 #include <linked_list_helper.h>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include "nondet.h"
 
 int main() {
-=======
-void aws_linked_list_next_harness() {
->>>>>>> be9808f... Add linked_list reset and next
-=======
-#include "nondet.h"
-
-int main() {
->>>>>>> f3cf922... Update linked_list_next
     /* data structure */
     struct aws_linked_list list;
 
@@ -22,33 +12,17 @@ int main() {
     struct aws_linked_list_node after; // Preconditions require after to not be NULL
 
     /* Assume the preconditions */
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f3cf922... Update linked_list_next
     void *front = nd_voidp();
     void *back = nd_voidp();
 
     list.head.prev = NULL;
     list.head.next = nd_voidp();
     node.prev = front;
-<<<<<<< HEAD
     node.next = &after;
     after.prev = &node;
     after.next = back;
     list.tail.prev = nd_voidp();
     list.tail.next = NULL;
-=======
-    node.next = &after;
-    after.prev = &node;
->>>>>>> be9808f... Add linked_list reset and next
-=======
-    node.next = &after;
-    after.prev = &node;
-    after.next = back;
-    list.tail.prev = nd_voidp();
-    list.tail.next = NULL;
->>>>>>> f3cf922... Update linked_list_next
 
     /* perform operation under verification */
     struct aws_linked_list_node *rval = aws_linked_list_next(&node);
@@ -57,10 +31,6 @@ int main() {
     sassert(aws_linked_list_node_next_is_valid(&node));
     sassert(aws_linked_list_node_prev_is_valid(rval));
     sassert(rval == &after);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> f3cf922... Update linked_list_next
 
     sassert(rval->prev == &node);
     sassert(rval->next == back);
@@ -73,9 +43,4 @@ int main() {
     sassert(list.tail.next == NULL);
 
     return 0;
-<<<<<<< HEAD
-=======
->>>>>>> be9808f... Add linked_list reset and next
-=======
->>>>>>> f3cf922... Update linked_list_next
 }
--- seahorn/jobs/linked_list_reset/aws_linked_list_reset_harness.c
+++ seahorn/jobs/linked_list_reset/aws_linked_list_reset_harness.c
@@ -2,9 +2,6 @@
 #include <aws/common/linked_list.h>
 #include <linked_list_helper.h>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include "nondet.h"
 
 int main () {
@@ -14,27 +11,6 @@ int main () {
     void *back = nd_voidp();
     node.next = front;
     node.prev = back;
-=======
-void aws_linked_list_node_reset_harness() {
-=======
-=======
-#include "nondet.h"
-
->>>>>>> f3cf922... Update linked_list_next
-int main () {
->>>>>>> fbd5cf8... Update linked_list_reset
-    /* data structure */
-    struct aws_linked_list_node node; // Preconditions require node to not be NULL
-<<<<<<< HEAD
-    node.next = nd_voidp();
-    node.prev = nd_voidp();
->>>>>>> be9808f... Add linked_list reset and next
-=======
-    void *front = nd_voidp();
-    void *back = nd_voidp();
-    node.next = front;
-    node.prev = back;
->>>>>>> 56b5555... Update linked_list_reset
     
     /* perform operation under verification */
     aws_linked_list_node_reset(&node);
@@ -43,14 +19,6 @@ int main () {
     sassert(AWS_IS_ZEROED(node));
     sassert(node.next == NULL);
     sassert(node.prev == NULL);
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     return 0;
-=======
->>>>>>> be9808f... Add linked_list reset and next
-=======
-
-    return 0;
->>>>>>> fbd5cf8... Update linked_list_reset
 }
