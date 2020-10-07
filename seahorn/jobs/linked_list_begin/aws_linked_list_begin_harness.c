7d8a5204c877f01c6340d7ccb16eae9fa8bca9e4
Emmy Ni <emmyni220@gmail.com>
2020-10-06 22:48:17 -0400

    Update linked_list_begin

--- seahorn/jobs/linked_list_begin/aws_linked_list_begin_harness.c
+++ seahorn/jobs/linked_list_begin/aws_linked_list_begin_harness.c
@@ -20,8 +20,13 @@ int main () {
     sassert(aws_linked_list_node_prev_is_valid(rval));
     sassert(aws_linked_list_node_next_is_valid(rval));
 <<<<<<< HEAD
+<<<<<<< HEAD
     return 0;
 =======
     sassert(aws_linked_list_is_valid(&list));
 >>>>>>> ac80371... Add linked_list int test
+=======
+
+    return 0;
+>>>>>>> c5d1a65... Update linked_list_begin
 }
