85c8b615570df72d64089d40e6e9f78ef4c27d4c
Emmy Ni <emmyni220@gmail.com>
2020-10-06 20:08:58 -0400

    Add linked_list_end

--- seahorn/jobs/linked_list_end/aws_linked_list_end_harness.c
+++ seahorn/jobs/linked_list_end/aws_linked_list_end_harness.c
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <seahorn/seahorn.h>
 #include <aws/common/linked_list.h>
 #include <linked_list_helper.h>
 
 int main() {
-<<<<<<< HEAD
-=======
-=======
-#include <seahorn/seahorn.h>
->>>>>>> be9808f... Add linked_list reset and next
-#include <aws/common/linked_list.h>
-#include <linked_list_helper.h>
-
-void aws_linked_list_end_harness() {
->>>>>>> d7da261... Add linked_list_end
-=======
->>>>>>> 773666d... Update linked_list_end
     /* data structure */
     struct aws_linked_list list;
     size_t length;
@@ -30,25 +16,9 @@ void aws_linked_list_end_harness() {
 
     /* assertions */
     sassert(rval == &list.tail);
-<<<<<<< HEAD
-<<<<<<< HEAD
     sassert(sea_aws_linked_list_is_valid(&list, length));
     sassert(aws_linked_list_node_prev_is_valid(rval));
     sassert(rval->next == NULL);
 
     return 0;
-=======
-=======
-    sassert(sea_aws_linked_list_is_valid(&list, length));
-    sassert(aws_linked_list_node_prev_is_valid(rval));
-<<<<<<< HEAD
-    sassert(aws_linked_list_node_next_is_valid(rval));
->>>>>>> ac80371... Add linked_list int test
-    sassert(aws_linked_list_is_valid(&list));
->>>>>>> d7da261... Add linked_list_end
-=======
-    sassert(rval->next == NULL);
-
-    return 0;
->>>>>>> 773666d... Update linked_list_end
 }
