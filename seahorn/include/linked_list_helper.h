9570feaf99d93ba497d59eba6bf9787378faf049
Daniel Zhou <danzhou1810@gmail.com>
2020-10-13 00:39:36 -0400

    refactor(jobs): migrate remaining linked list jobs to nd helpers
    (draft): rewrite nd full linked list save and unchanged functions
    
    code cleanup

--- seahorn/include/linked_list_helper.h
+++ seahorn/include/linked_list_helper.h
@@ -42,6 +42,21 @@ void sea_nd_init_aws_linked_list_from_head(struct aws_linked_list *list,
 void sea_nd_init_aws_linked_list_from_tail(struct aws_linked_list *list,
                                            size_t *length);
 
+/*
+    For functions that only access head and tail
+    Non-deterministically initialize aws_linked_list of the forms:
+    If size == 0, then
+    HEAD <--> TAIL
+    If size == 1, then
+    HEAD <--> FRONT <--> TAIL
+    If size == 2, then
+    HEAD <--> FRONT <--> BACK <--> TAIL
+    If size > 2, then
+    HEAD <--> FRONT --> nd_ptr ... nd_ptr <-- BACK <--> TAIL
+*/
+void sea_nd_init_aws_linked_list(struct aws_linked_list *list,
+                                 size_t *length);
+
 // This stores a snapshot of a single linked list node
 struct saved_aws_linked_list_node {
   struct aws_linked_list_node *node;
@@ -55,11 +70,15 @@ struct saved_aws_linked_list {
   size_t saved_size;
   // the node from where to start saving either forwards or backwards
   struct aws_linked_list_node *save_point;
+  // optional save point for saving from both sides
+  struct aws_linked_list_node *save_point_end;
   struct saved_aws_linked_list_node head;
   struct saved_aws_linked_list_node tail;
-  // save upto 3 nodes - head, node1, node2 as needed OR
+  // one direction saves: save upto 3 nodes - head, node1, node2 as needed OR
   // node1, node2, tail as needed
-  struct saved_aws_linked_list_node nodes[3];
+  // bi-direction saves: save up to 4 nodes:
+  // head<->front...back->tail
+  struct saved_aws_linked_list_node nodes[4];
 };
 
 // returns true if the final list is unmodified from start to tail
@@ -79,6 +98,13 @@ bool is_aws_list_unchanged_to_tail(struct aws_linked_list *list,
 bool is_aws_list_unchanged_to_head(struct aws_linked_list *list,
                                    struct saved_aws_linked_list *saved);
 
+// returns true if the final aw_linked_list *list* is unmodified from
+// saved_point to saved_point_end when checking against snapshot
+// *saved*.
+// To be used when *saved* is created using aws_linked_list_save_full
+bool is_aws_list_unchanged_full(struct aws_linked_list *list,
+                                struct saved_aws_linked_list *saved);
+
 // store a snapshot of a sub linked list starting with the node marked
 // 'start'. To be used in when list is created using
 // sea_nd_init_aws_linked_list_from_head(...)
@@ -92,6 +118,13 @@ void aws_linked_list_save_to_tail(struct aws_linked_list *list, size_t size,
 void aws_linked_list_save_to_head(struct aws_linked_list *list, size_t size,
                                   struct aws_linked_list_node *start,
                                   struct saved_aws_linked_list *to_save);
+
+// store full snapshot of *list* to *to_save*; only save 1 level deep from both
+// head and tail, i.e. head.next and tail.prev
+void aws_linked_list_save_full(struct aws_linked_list *list, size_t size,
+                               struct aws_linked_list_node *start,
+                               struct aws_linked_list_node *end,
+                               struct saved_aws_linked_list *to_save);
 
 bool is_aws_linked_list_node_attached_after(
     struct aws_linked_list_node *after, struct aws_linked_list_node *to_attach);
