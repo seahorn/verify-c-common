a8933b23957b760734d66a585ab8742d7bf6ecda
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-10-26 11:40:45 -0400

    refactor(lib): merge definitions of aws_raise_error_private

--- seahorn/lib/utils.c
+++ seahorn/lib/utils.c
@@ -154,14 +154,6 @@ const char *ensure_c_str_is_allocated(size_t max_size) {
   return str;
 }
 
-#ifndef aws_raise_error_private
-void aws_raise_error_private(int err) {
-#ifdef AWS_ERROR_IS_VERIFIER_ERROR
-  __VERIFIER_error();
-#elif AWS_ERROR_IS_EXIT
-  exit(1)
-#else
-  return;
-#endif
-}
+#ifdef __SEAHORN__
+#include "error.c"
 #endif
