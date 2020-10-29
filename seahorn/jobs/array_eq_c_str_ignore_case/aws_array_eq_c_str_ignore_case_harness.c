2c713b0da0a88719769da53e5268d4afaa5eb071
Siddharth Priya <priya.siddharth@gmail.com>
2020-10-29 08:29:28 -0400

    refactor(utils): allow different sizes during string alloc

--- seahorn/jobs/array_eq_c_str_ignore_case/aws_array_eq_c_str_ignore_case_harness.c
+++ seahorn/jobs/array_eq_c_str_ignore_case/aws_array_eq_c_str_ignore_case_harness.c
@@ -9,24 +9,25 @@
 #include <utils.h>
 
 int main() {
-    /* assumptions */
-    size_t array_len = nd_size_t();
-    assume(array_len <= MAX_BUFFER_SIZE);
-    void *array = can_fail_malloc(MAX_BUFFER_SIZE);
-    size_t str_len = 0;
-    const char *c_str = ensure_c_str_is_nd_allocated(MAX_BUFFER_SIZE, &str_len);
+  /* assumptions */
+  size_t array_len = nd_size_t();
+  assume(array_len <= MAX_BUFFER_SIZE);
+  void *array = can_fail_malloc(MAX_BUFFER_SIZE);
+  size_t str_len = 0;
+  const char *c_str =
+      ensure_c_str_is_nd_allocated_safe(MAX_BUFFER_SIZE, &str_len);
 
-    /* save current state of the parameters */
-    struct store_byte_from_buffer old_byte_from_array;
-    save_byte_from_array((uint8_t *)array, array_len, &old_byte_from_array);
-    struct store_byte_from_buffer old_byte_from_str;
-    save_byte_from_array((uint8_t *)c_str, str_len, &old_byte_from_str);
+  /* save current state of the parameters */
+  struct store_byte_from_buffer old_byte_from_array;
+  save_byte_from_array((uint8_t *)array, array_len, &old_byte_from_array);
+  struct store_byte_from_buffer old_byte_from_str;
+  save_byte_from_array((uint8_t *)c_str, str_len, &old_byte_from_str);
 
-    /* pre-conditions */
-    assume(array || (array_len == 0));
-    assume(c_str);
+  /* pre-conditions */
+  assume(array || (array_len == 0));
+  assume(c_str);
 
-    /* operation under verification */
+  /* operation under verification */
     if (aws_array_eq_c_str_ignore_case(array, array_len, c_str)) {
         sassert(array_len == str_len);
     }
