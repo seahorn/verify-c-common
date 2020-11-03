1dd2ad4a6732a59fc142bf35cdb478f2c43a3f64
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-11-02 22:39:38 -0500

    fix(lib): string_helpers should not depend on MAX_STRING_LEN
    The dependence was introduced originally because SeaHorn supported only literal
    allocation constants. Changed back to symbolic constants since they are supported.

--- seahorn/lib/string_helper.c
+++ seahorn/lib/string_helper.c
@@ -3,13 +3,15 @@
 #include <proof_allocators.h>
 
 struct aws_string *ensure_string_is_allocated(size_t len) {
-    struct aws_string *str = bounded_malloc(sizeof(struct aws_string) + MAX_STRING_LEN + 1);
-    assume(len <= MAX_STRING_LEN);
+    struct aws_string *str = bounded_malloc(sizeof(struct aws_string) + len + 1);
 
-    /* Fields are declared const, so we need to copy them in like this */
-    *(struct aws_allocator **)(&str->allocator) = nd_bool() ? nd_voidp() : NULL;
-    *(size_t *)(&str->len) = len;
-    *(uint8_t *)&str->bytes[len] = '\0';
+    if (str) {
+      /* Fields are declared const, so we need to copy them in like this */
+      *(struct aws_allocator **)(&str->allocator) =
+          nd_bool() ? nd_voidp() : NULL;
+      *(size_t *)(&str->len) = len;
+      *(uint8_t *)&str->bytes[len] = '\0';
+    }
     return str;
 }
 
