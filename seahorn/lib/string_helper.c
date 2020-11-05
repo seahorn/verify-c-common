8b3f4d8a645a6d846340fd325bd43d33ab1bf6ca
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-11-04 22:27:37 -0500

    refactor(lib): clean up string_helper
    Hide local methods.
    
    Make definitions more explicit.

--- seahorn/lib/string_helper.c
+++ seahorn/lib/string_helper.c
@@ -1,59 +1,68 @@
+#include <bounds.h>
+#include <proof_allocators.h>
+#include <sea_allocators.h>
 #include <seahorn/seahorn.h>
 #include <string_helper.h>
-#include <proof_allocators.h>
+
+static void sea_init_str(char *str, size_t str_len);
 
 struct aws_string *ensure_string_is_allocated(size_t len) {
-    struct aws_string *str = bounded_malloc(sizeof(struct aws_string) + len + 1);
+  struct aws_string *str = sea_malloc_havoc_safe(sizeof(struct aws_string) + len + 1);
 
-    if (str) {
-      /* Fields are declared const, so we need to copy them in like this */
-      *(struct aws_allocator **)(&str->allocator) =
-          nd_bool() ? nd_voidp() : NULL;
-      *(size_t *)(&str->len) = len;
-      *(uint8_t *)&str->bytes[len] = '\0';
-    }
-    return str;
+  if (str) {
+    /* Fields are declared const, so we need to copy them in like this */
+    *(struct aws_allocator **)(&str->allocator) = nd_bool() ? sea_allocator() : NULL;
+    *(size_t *)(&str->len) = len;
+    *(uint8_t *)&str->bytes[len] = '\0';
+  }
+  return str;
 }
 
 struct aws_string *ensure_string_is_allocated_bounded_length(size_t max_size) {
-    size_t len = nd_size_t();
-    assume(len < max_size);
-    return ensure_string_is_allocated(len);
+  size_t len = nd_size_t();
+  assume(len < max_size);
+  return ensure_string_is_allocated(len);
 }
 
 struct aws_string *ensure_string_is_allocated_nondet_length(void) {
-    /* Considers any size up to the maximum possible size for the array [bytes] in aws_string */
-    return ensure_string_is_allocated_bounded_length(SIZE_MAX - 1 - sizeof(struct aws_string));
+  /* Considers any size up to the maximum possible size for the array [bytes] in
+   * aws_string */
+  return ensure_string_is_allocated_bounded_length(SIZE_MAX - 1 -
+                                                   sizeof(struct aws_string));
 }
 
 // This takes in a ptr to an allocated piece of memory with the desired
 // string len and initializes the string.
-void sea_init_str(const char *str, size_t str_len) {
-  assume(str[str_len] == '\0');
-  // The following assumption cannot be expressed
-  // assume(forall j :: j < i ==> str[j] != '\0');
-  // therefore, we say the following:
-  for (size_t j = 0; j < MAX_STRING_LEN; j++) {
+static void sea_init_str(char *str, size_t str_len) {
+  str[str_len] = '\0';
+  size_t max_string_len = sea_max_string_len();
+  for (size_t j = 0; j < max_string_len; j++) {
     if (j < str_len) {
       assume(str[j] != '\0');
     }
   }
 }
 
-// This cannot return a NULL ptr.
-// Returned len is always > 0
-const char *ensure_c_str_is_nd_allocated_safe(size_t max_size, size_t *len) {
+static const char *_ensure_c_str_is_nd_allocated(size_t max_size, size_t *len,
+                                          bool safe) {
   size_t alloc_size;
   alloc_size = nd_size_t();
   // allocate no more than MAX_STRING_LEN + 1
-  assume(alloc_size > 0 && alloc_size <= max_size &&
-         max_size <= MAX_STRING_LEN);
-  // bounded_malloc never fails
-  const char *str = bounded_malloc(alloc_size);
-  if (!str) {
-    *len = 0;
-    return str;
+  assume(alloc_size > 0);
+  assume(alloc_size <= max_size);
+  assume(max_size <= MAX_STRING_LEN);
+
+  // this allocation never fails
+  char *str;
+  if (safe) {
+    str = sea_malloc_havoc_safe(alloc_size);
+  } else {
+    str = sea_malloc_havoc(alloc_size);
+    if (!str) {
+      return NULL;
+    }
   }
+
   sea_init_str(str, alloc_size - 1);
   *len = alloc_size - 1;
   return str;
@@ -61,17 +70,10 @@ const char *ensure_c_str_is_nd_allocated_safe(size_t max_size, size_t *len) {
 
 // This can return a NULL ptr. len will be zero in this case.
 const char *ensure_c_str_is_nd_allocated(size_t max_size, size_t *len) {
-  size_t alloc_size;
-  alloc_size = nd_size_t();
-  assume(alloc_size > 0 && alloc_size <= max_size &&
-         max_size <= MAX_STRING_LEN);
-  const char *str = can_fail_malloc(alloc_size);
-  if (!str) {
-    *len = 0;
-    return str;
-  }
-  sea_init_str(str, alloc_size - 1);
-  *len = alloc_size - 1;
-  return str;
+  return _ensure_c_str_is_nd_allocated(max_size, len, false);
 }
 
+// This can return a NULL ptr. len will be zero in this case.
+const char *ensure_c_str_is_nd_allocated_safe(size_t max_size, size_t *len) {
+  return _ensure_c_str_is_nd_allocated(max_size, len, true);
+}
