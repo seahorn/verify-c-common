0a56602b2c24267eada3b4c34abd14c477bb1882
Siddharth Priya <priya.siddharth@gmail.com>
2020-11-17 10:10:08 -0500

    feat: use static loop unrolling for strlen
    Use C++ templates to unroll loop at compile time and then link with rest of the C code.

---
+++ seahorn/include/macros.h
@@ -0,0 +1,7 @@
+#ifdef __cplusplus
+#    define SEAHORN_EXTERN_C_BEGIN extern "C" {
+#    define SEAHORN_EXTERN_C_END }
+#else
+#    define SEAHORN_EXTERN_C_BEGIN
+#    define SEAHORN_EXTERN_C_END
+#endif /*  __cplusplus */
