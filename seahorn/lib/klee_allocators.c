792703c486de39a347b616d70dcb2f3f13f5de35
Yusen Su <y256su@uwaterloo.ca>
2020-11-17 11:01:46 +0000

    feat(lib): defined sea_allocators adapted to klee

---
+++ seahorn/lib/klee_allocators.c
@@ -0,0 +1,58 @@
+
+#include <proof_allocators.h>
+
+size_t alloc_size(size_t sz) {
+  switch(sz) {
+    case 1: return 1;
+    case 2: return 2;
+    case 3: return 3;
+    case 4: return 4;
+    case 5: return 5;
+    case 6: return 6;
+    case 7: return 7;
+    case 8: return 8;
+    case 9: return 9;
+    case 10: return 10;
+    case 11: return 11;
+    case 12: return 12;
+    case 13: return 13;
+    case 14: return 14;
+    case 15: return 15;
+    case 16: return 16;
+    default: return sz <= 4096 ? 4096 : sz; 
+  }
+}
+
+void *bounded_malloc(size_t size) {
+  return malloc(alloc_size(size));
+}
+
+void *can_fail_malloc(size_t size) { 
+    return malloc(alloc_size(size)); 
+}
+
+/**
+ *
+ */
+static void *s_malloc_allocator(struct aws_allocator *allocator, size_t size) {
+  (void)allocator;
+  return bounded_malloc(size);
+}
+
+/**
+ */
+static void s_free_allocator(struct aws_allocator *allocator, void *ptr) {
+  (void)allocator;
+  free(ptr);
+}
+
+static struct aws_allocator s_allocator_klee_static = {
+    .mem_acquire = s_malloc_allocator,
+    .mem_release = s_free_allocator,
+    .mem_realloc = NULL,
+    .mem_calloc = NULL,
+};
+
+struct aws_allocator *sea_allocator() {
+  return &s_allocator_klee_static;
+}
