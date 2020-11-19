db7a8aeace0e48bc1bb13907c18c6196465b49c7
Yusen Su <y256su@uwaterloo.ca>
2020-11-19 09:30:46 +0000

    refactor(array list): move bounded functions into array_list_helper and documented

--- seahorn/lib/array_list_helper.c
+++ seahorn/lib/array_list_helper.c
@@ -19,6 +19,27 @@ void initialize_array_list(struct aws_array_list *const list) {
   list->data = can_fail_malloc(list->current_size);
   list->alloc = sea_allocator();
 }
+
+void failed_allocation_after_initialize(struct aws_array_list *const list) {
+  if (list->data == NULL) {
+    list->current_size = 0;
+    list->length = 0;
+  }
+}
+
+void initialize_bounded_array_list(struct aws_array_list *const list,
+                                const size_t max_block_size) {
+  list->current_size = nd_size_t();
+  assume(list->current_size <= max_block_size);
+  list->item_size = nd_size_t();
+  list->length = nd_size_t();
+  assume(list->length * list->item_size <= max_block_size);
+  assume(list->current_size >= list->length * list->item_size);
+  list->data = can_fail_malloc(list->current_size);
+  list->alloc = sea_allocator();
+  failed_allocation_after_initialize(list);
+}
+
 bool aws_array_list_is_bounded(const struct aws_array_list *const list,
                                const size_t max_initial_item_allocation,
                                const size_t max_item_size) {
