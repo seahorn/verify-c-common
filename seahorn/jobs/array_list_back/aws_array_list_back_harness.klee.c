e2c6883767ef5fa26d3a226afb49494759fccb33
Yusen Su <y256su@uwaterloo.ca>
2020-11-19 09:32:40 +0000

    refactor(harness): rename bounded array list harness

--- seahorn/jobs/array_list_back/aws_array_list_back_harness.klee.c
+++ seahorn/jobs/array_list_back/aws_array_list_back_harness.klee.c
@@ -4,9 +4,23 @@
 
 #include <seahorn/seahorn.h>
 #include <aws/common/array_list.h>
-#include <bounded_array_list_helper.h>
+#include <array_list_helper.h>
 #include <utils.h>
 #include <proof_allocators.h>
+
+bool aws_array_list_is_valid_copy(const struct aws_array_list *AWS_RESTRICT list) {
+    if (!list) {
+        return false;
+    }
+    size_t required_size = 0;
+    bool required_size_is_valid =
+        (aws_mul_size_checked(list->length, list->item_size, &required_size) == AWS_OP_SUCCESS);
+    bool current_size_is_valid = (list->current_size >= required_size);
+    bool data_is_valid = AWS_IMPLIES(list->current_size == 0, list->data == NULL) &&
+                         AWS_IMPLIES(list->current_size != 0, AWS_MEM_IS_WRITABLE(list->data, list->current_size));
+    bool item_size_is_valid = (list->item_size != 0);
+    return required_size_is_valid && current_size_is_valid && data_is_valid && item_size_is_valid;
+}
 
 /**
  * Runtime: 19s
