2fcf897ac067bc4d7b553efe1e9c18f0602fe294
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-11-23 10:47:21 -0500

    fix(job): remove work around sea-dsa bug

--- seahorn/jobs/array_list_comparator_string/aws_array_list_comparator_string_harness.c
+++ seahorn/jobs/array_list_comparator_string/aws_array_list_comparator_string_harness.c
@@ -14,11 +14,6 @@ int main() {
     struct aws_string *str_a = ensure_string_is_allocated_bounded_length(MAX_BUFFER_SIZE);
     struct aws_string *str_b = nd_bool() ? str_a : ensure_string_is_allocated_bounded_length(MAX_BUFFER_SIZE);
 
-    // -- force aliasing between the two strings
-    // -- without this annotation, type-aware sea-dsa gets confused between different representations
-    // -- of the string and loses some aliasing relationships
-    sea_dsa_alias((void*)str_a, (void*)str_b);
-
     bool nondet_parameter_a = nd_bool();
     bool nondet_parameter_b = nd_bool();
     if (aws_array_list_comparator_string(nondet_parameter_a ? &str_a : NULL, nondet_parameter_b ? &str_b : NULL) == 0) {
