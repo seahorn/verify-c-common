c1f256ab8481e343ab8e4b9546b8ee9745007a41
Yusen Su <y256su@uwaterloo.ca>
2020-11-30 14:19:49 +0000

    fix(lib): redesign memhavoc

--- seahorn/lib/klee_allocators.c
+++ seahorn/lib/klee_allocators.c
@@ -1,21 +1,7 @@
 
 #include <proof_allocators.h>
+#include <klee_switch.h>
 #include <nondet.h>
-size_t alloc_size(size_t sz) {
-  switch(sz) {
-    case 1: return 1;
-    case 2: return 2;
-    case 3: return 3;
-    case 4: return 4;
-    case 5: return 5;
-    case 6: return 6;
-    case 7: return 7;
-    case 8: return 8;
-    case 9: return 9;
-    case 10: return 10;
-    default: return sz <= 4096 ? 4096 : sz;
-  }
-}
 
 void *bounded_malloc(size_t size) {
   return size == 0 ? NULL : malloc(alloc_size(size));
