62c04b6f2298e66396ae22065dc2347311e8572b
Yusen Su <y256su@uwaterloo.ca>
2020-11-30 14:19:49 +0000

    fix(lib): redesign memhavoc

--- seahorn/include/bounds.h
+++ seahorn/include/bounds.h
@@ -11,3 +11,18 @@ size_t sea_max_buffer_size(void);
    Returns the current upper bound on string size
  */
 size_t sea_max_string_len(void);
+
+/**
+   Returns the current upper bound on array list initial size
+ */
+size_t sea_max_array_list_initial_size(void);
+
+/**
+   Returns the current upper bound on array list item size
+ */
+size_t sea_max_array_list_item_size(void);
+
+/**
+   Returns the current upper bound on array list length
+ */
+size_t sea_max_array_list_len(void);
