e20ca56b4ae532ed11d8a48805ab032e4a650e94
Yusen Su <y256su@uwaterloo.ca>
2020-11-30 14:19:49 +0000

    fix(lib): redesign memhavoc

--- seahorn/lib/bounds.c
+++ seahorn/lib/bounds.c
@@ -5,8 +5,23 @@
 #endif
 
 #ifndef MAX_STRING_LEN
-#define MAX_STRING_LEN MAX_BUFFER_SZIE
+#define MAX_STRING_LEN MAX_BUFFER_SIZE
+#endif
+
+#ifndef MAX_ITEM_SIZE
+#define MAX_ITEM_SIZE 2
+#endif
+
+#ifndef MAX_INITIAL_SIZE
+#define MAX_INITIAL_SIZE MAX_BUFFER_SIZE
+#endif
+
+#ifndef MAX_INITIAL_ITEM_ALLOCATION
+#define MAX_INITIAL_ITEM_ALLOCATION 9223372036854775808ULL
 #endif
 
 size_t sea_max_buffer_size(void) { return MAX_BUFFER_SIZE; }
 size_t sea_max_string_len(void) { return MAX_STRING_LEN; }
+size_t sea_max_array_list_initial_size(void) { return MAX_INITIAL_SIZE; }
+size_t sea_max_array_list_item_size(void) { return MAX_ITEM_SIZE; }
+size_t sea_max_array_list_len(void) { return MAX_INITIAL_ITEM_ALLOCATION; }
--- seahorn/lib/nd_klee.c
+++ seahorn/lib/nd_klee.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
