988ac1fee6909cc7c244a7997aa88e7e5d1e3487
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-12-01 11:04:37 -0500

    feat(lib): model of aws_last_error

--- seahorn/lib/error_override.c
+++ seahorn/lib/error_override.c
@@ -1,9 +1,11 @@
+#include <aws/common/common.h>
+
+static AWS_THREAD_LOCAL int tl_last_error = 0;
+
 void aws_raise_error_private(int err) {
-#ifdef AWS_ERROR_IS_VERIFIER_ERROR
-  __VERIFIER_error();
-#elif AWS_ERROR_IS_EXIT
-  exit(1)
-#else
-  return;
-#endif
+  tl_last_error = err;
+}
+
+int aws_last_error(void) {
+  return tl_last_error;
 }
