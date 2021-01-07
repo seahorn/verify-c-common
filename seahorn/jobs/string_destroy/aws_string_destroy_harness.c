e1338750123dac4aed4e2194bcfbccb9f5a4f15a
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-01-07 16:23:20 -0500

    feat(job): aws_string_destroy

--- seahorn/jobs/string_destroy/aws_string_destroy_harness.c
+++ seahorn/jobs/string_destroy/aws_string_destroy_harness.c
@@ -3,16 +3,16 @@
  */
 
 #include <aws/common/string.h>
-#include <seahorn/seahorn.h>
 #include <byte_buf_helper.h>
+#include <seahorn/seahorn.h>
+#include <stddef.h>
 #include <string_helper.h>
 #include <utils.h>
-#include <stddef.h>
 
-int main() {
-    struct aws_string *str = nd_bool() ?
-            ensure_string_is_allocated_nondet_length() : NULL;
-    aws_string_destroy(str);
+int main(void) {
+  struct aws_string *str =
+      nd_bool() ? ensure_string_is_allocated_nondet_length() : NULL;
+  aws_string_destroy(str);
 
-    return 0;
+  return 0;
 }
