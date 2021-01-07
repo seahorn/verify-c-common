2fa3e4f537f9df59e0d924ad5ae8c53b80971667
Daniel Zhou <danzhou1810@gmail.com>
2021-01-07 00:24:07 -0500

    feat(jobs): add hash_array_ignore_case and hash_byte_cursor_ptr(bld edg)

---
+++ seahorn/jobs/hash_byte_cursor_ptr/aws_hash_byte_cursor_ptr_harness.c
@@ -0,0 +1,20 @@
+#include <seahorn/seahorn.h>
+#include <aws/common/hash_table.h>
+#include <aws/common/private/hash_table_impl.h>
+#include <aws/common/byte_buf.h>
+#include <byte_buf_helper.h>
+#include <proof_allocators.h>
+#include <utils.h>
+
+int main(void) {
+    struct aws_byte_cursor cur;
+    initialize_byte_cursor(&cur);
+
+    assume(aws_byte_cursor_is_bounded(&cur, MAX_BUFFER_SIZE));
+    assume(aws_byte_cursor_is_valid(&cur));
+
+    /* This function has no pre or post conditions */
+    uint64_t rval = aws_hash_byte_cursor_ptr(&cur);
+
+    sassert(aws_byte_cursor_is_valid(&cur));
+}
