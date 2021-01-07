450967b0d91a17c410b6580cb0190c0ad4764ff8
Daniel Zhou <danzhou1810@gmail.com>
2021-01-06 19:14:25 -0500

    feat(jobs): add hash_ptr and hash_string jobs

---
+++ seahorn/jobs/hash_ptr/aws_hash_ptr_harness.c
@@ -0,0 +1,11 @@
+#include <seahorn/seahorn.h>
+#include <aws/common/hash_table.h>
+#include <nondet.h>
+
+int main(void) {
+    /* hash value of ptr itself only, so deref should be illegal */
+    void *ptr = nd_voidp();
+    /* This function has no pre or post conditions */
+    /* passively checks mem safety */
+    uint64_t rval = aws_hash_ptr(ptr);
+}
