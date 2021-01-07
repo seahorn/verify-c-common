ba79a76d742fe516985c5a37c0fe7f5f44d9d359
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-01-07 16:40:24 -0500

    feat(job): string_destroy_secure
    In vac and cex mode, there is a message about unsupported inlined asm.
    Does not appear in normal mode because of COI that removes it.
    
    The instance might be sat with word-size of 8 because of unaligned memset that
    is not supported well. Something to investigate in the future

--- seahorn/jobs/string_destroy_secure/aws_string_destroy_secure_harness.c
+++ seahorn/jobs/string_destroy_secure/aws_string_destroy_secure_harness.c
@@ -3,23 +3,23 @@
  */
 
 #include <aws/common/string.h>
-#include <seahorn/seahorn.h>
 #include <byte_buf_helper.h>
+#include <seahorn/seahorn.h>
+#include <stddef.h>
 #include <string_helper.h>
 #include <utils.h>
-#include <stddef.h>
 
-int main() {
-    struct aws_string *str = ensure_string_is_allocated_bounded_length(MAX_STRING_LEN);
-    char const *bytes = str->bytes;
-    size_t len = str->len;
-    /* Tell CBMC to keep the buffer live after the free */
-    __CPROVER_allocated_memory(bytes, len);
-    bool nondet_parameter = nd_bool();
-    aws_string_destroy_secure(nondet_parameter ? str : NULL);
-    if (nondet_parameter) {
-        assert_all_zeroes(bytes, len);
-    }
-
-    return 0;
+int main(void) {
+  struct aws_string *str =
+      ensure_string_is_allocated_bounded_length(MAX_STRING_LEN);
+  char unsigned const *bytes = str->bytes;
+  size_t len = str->len;
+  /* Tell CBMC to keep the buffer live after the free */
+  /* __CPROVER_allocated_memory(bytes, len); */
+  bool nondet_parameter = nd_bool();
+  aws_string_destroy_secure(nondet_parameter ? str : NULL);
+  if (nondet_parameter) {
+    assert_all_zeroes(bytes, len);
+  }
+  return 0;
 }
