191e2ffd145c672edaf5d0acb6c490fc39d1469d
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-01-08 12:23:11 -0500

    feat(lib): initialization for aws_hash_iter

--- seahorn/include/hash_table_helper.h
+++ seahorn/include/hash_table_helper.h
@@ -9,9 +9,13 @@
  */
 void initialize_bounded_aws_hash_table(struct aws_hash_table *map,
                                        size_t max_table_entries);
+void initialize_aws_hash_iter(struct aws_hash_iter *iter,
+                              struct aws_hash_table *map);
+void mk_valid_aws_hash_iter(struct aws_hash_iter *iter,
+                            struct aws_hash_table *map);
 
 void save_byte_from_hash_table(const struct aws_hash_table *map,
                                struct store_byte_from_buffer *storage);
 
 void assert_hash_table_unchanged(const struct aws_hash_table *map,
-                                const struct store_byte_from_buffer *storage);
+                                 const struct store_byte_from_buffer *storage);
--- seahorn/lib/hash_table_helper.c
+++ seahorn/lib/hash_table_helper.c
@@ -14,12 +14,28 @@ void initialize_bounded_aws_hash_table(struct aws_hash_table *map,
 
   size_t required_bytes = nd_size_t();
   /* assume setting required_bytes is successful */
-  assume(!hash_table_state_required_bytes(num_entries, &required_bytes));
+  assume(hash_table_state_required_bytes(num_entries, &required_bytes) == AWS_OP_SUCCESS);
   struct hash_table_state *impl = bounded_malloc(required_bytes);
   impl->size = num_entries;
   map->p_impl = impl;
 }
 
+extern NONDET_FN_ATTR enum aws_hash_iter_status nd_hash_iter_status(void);
+void initialize_aws_hash_iter(struct aws_hash_iter *iter,
+                              struct aws_hash_table *map) {
+  iter->map = map;
+  iter->element.key = nd_voidp();
+  iter->element.value = nd_voidp();
+  iter->slot = nd_size_t();
+  iter->limit = nd_size_t();
+  iter->status = nd_hash_iter_status();
+}
+
+void mk_valid_aws_hash_iter(struct aws_hash_iter *iter,
+                            struct aws_hash_table *map) {
+  initialize_aws_hash_iter(iter, map);
+  assume(aws_hash_iter_is_valid(iter));
+}
 void save_byte_from_hash_table(const struct aws_hash_table *map,
                                struct store_byte_from_buffer *storage) {
   struct hash_table_state *state = map->p_impl;
