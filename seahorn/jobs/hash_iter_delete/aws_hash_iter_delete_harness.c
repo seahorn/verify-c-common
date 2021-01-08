395155cca5a7b1c33176077d028b6688b41223f8
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-01-08 15:37:40 -0500

    feat(job): aws_hash_iter_delete

---
+++ seahorn/jobs/hash_iter_delete/aws_hash_iter_delete_harness.c
@@ -0,0 +1,51 @@
+/**
+ */
+
+#include <aws/common/hash_table.h>
+#include <aws/common/private/hash_table_impl.h>
+
+#include <seahorn/seahorn.h>
+
+#include <config.h>
+#include <hash_table_helper.h>
+#include <proof_allocators.h>
+#include <utils.h>
+
+bool hash_table_state_has_an_empty_slot(
+    const struct hash_table_state *const state, size_t *const rval) {
+  assume(state->entry_count > 0);
+  size_t empty_slot_idx = nd_size_t();
+  assume(empty_slot_idx < state->size);
+  *rval = empty_slot_idx;
+  return state->slots[empty_slot_idx].hash_code == 0;
+}
+bool aws_hash_table_has_an_empty_slot(const struct aws_hash_table *const map,
+                                      size_t *const rval) {
+  return hash_table_state_has_an_empty_slot(map->p_impl, rval);
+}
+
+void hash_proof_destroy_noop(void *p) {}
+
+int main(void) {
+  struct aws_hash_table map;
+  initialize_bounded_aws_hash_table(&map, MAX_TABLE_SIZE);
+  assume(aws_hash_table_is_valid(&map));
+  assume(map.p_impl->destroy_key_fn == hash_proof_destroy_noop ||
+         map.p_impl->destroy_key_fn == NULL);
+  assume(map.p_impl->destroy_value_fn == hash_proof_destroy_noop ||
+         map.p_impl->destroy_value_fn == NULL);
+
+  size_t empty_slot_idx;
+  assume(aws_hash_table_has_an_empty_slot(&map, &empty_slot_idx));
+
+  struct hash_table_state *state = map.p_impl;
+
+  struct aws_hash_iter iter;
+  mk_valid_aws_hash_iter(&iter, &map);
+  assume(iter.status == AWS_HASH_ITER_STATUS_READY_FOR_USE);
+
+  aws_hash_iter_delete(&iter, nd_bool());
+  sassert(aws_hash_iter_is_valid(&iter));
+  sassert(iter.status == AWS_HASH_ITER_STATUS_DELETE_CALLED);
+  return 0;
+}
