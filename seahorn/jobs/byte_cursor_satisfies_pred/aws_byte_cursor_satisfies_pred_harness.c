d746a2cd8ac4b699fab91ad41406889d27a48f4b
LinerSu <ys3547@nyu.edu>
2021-01-16 11:18:01 -0500

    feat(jobs): add missed byte cursor jobs from aws-c-common

---
+++ seahorn/jobs/byte_cursor_satisfies_pred/aws_byte_cursor_satisfies_pred_harness.c
@@ -0,0 +1,32 @@
+/**
+ *
+ */
+
+#include <seahorn/seahorn.h>
+#include <aws/common/byte_buf.h>
+#include <proof_allocators.h>
+#include <byte_buf_helper.h>
+#include <utils.h>
+
+int main() {
+    /* parameters */
+    struct aws_byte_cursor cur;
+    initialize_byte_cursor(&cur);
+
+    /* assumptions */
+    assume(aws_byte_cursor_is_bounded(&cur, MAX_BUFFER_SIZE));
+    assume(aws_byte_cursor_is_valid(&cur));
+
+    /* save current state of the data structure */
+    struct store_byte_from_buffer old_byte_from_cur;
+    save_byte_from_array(cur.ptr, cur.len, &old_byte_from_cur);
+
+    /* operation under verification */
+    aws_byte_cursor_satisfies_pred(&cur, uninterpreted_predicate_fn);
+
+    /* assertions */
+    sassert(aws_byte_cursor_is_valid(&cur));
+    if (cur.len > 0) {
+        assert_byte_from_buffer_matches(cur.ptr, &old_byte_from_cur);
+    }
+}
