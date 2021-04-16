17e963209b3f4ed8203b21de4b32fb964a9553ea
yusen <y256su@uwaterloo.ca>
2021-04-16 09:47:56 +0000

    feat(lib): add nd func and allocators for smack

---
+++ seahorn/lib/smack_allocators.c
@@ -0,0 +1,45 @@
+/*
+ *
+ */
+
+#include <proof_allocators.h>
+
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+
+void *bounded_malloc(size_t size) {
+    return size == 0 ? NULL : malloc(size);
+}
+
+void *can_fail_malloc(size_t size) { 
+    return size == 0 ? NULL : malloc(size); 
+}
+
+/**
+ *
+ */
+static void *s_malloc_allocator(struct aws_allocator *allocator, size_t size) {
+  (void)allocator;
+  return bounded_malloc(size);
+}
+
+/**
+ */
+static void s_free_allocator(struct aws_allocator *allocator, void *ptr) {
+  (void)allocator;
+  free(ptr);
+}
+
+static struct aws_allocator s_allocator_smack_static = {
+    .mem_acquire = s_malloc_allocator,
+    .mem_release = s_free_allocator,
+    .mem_realloc = NULL,
+    .mem_calloc = NULL,
+};
+
+struct aws_allocator *sea_allocator() {
+  return &s_allocator_smack_static;
+}
+
