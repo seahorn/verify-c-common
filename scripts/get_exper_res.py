b925c0b2876b9c6e7defb11596c2d10e785ba767
yusen <y256su@uwaterloo.ca>
2021-05-10 14:41:00 +0000

    feat(scripts): add smack into experimental script

--- scripts/get_exper_res.py
+++ scripts/get_exper_res.py
@@ -9,7 +9,7 @@ from get_exper_brunch_stat import read_brunchstat_from_log, write_brunchstat_int
 
 BUILDABSPATH = os.path.abspath('../build/')
 DATAABSPATH = os.path.abspath('../') + "/data"
-SEAHORN_ROOT = '../../seahorn/build/run'  # Put your seahorn root dir here
+SEAHORN_ROOT = '../../build-seahorn'  # Put your seahorn root dir here
 FILE_DICT = {
     "": "seahorn.csv",
     "--vac": "seahorn(vac).csv",
@@ -27,9 +27,15 @@ def get_output_level():
     return output_level
 
 
-def make_new_cmake_conf(use_klee, use_bleeding_edge):
+def make_new_cmake_conf():
+    use_klee = "ON" if args.klee else "OFF"
+    use_bleeding_edge = "ON" if args.bleed_edge else "OFF"
+    use_smack = "ON" if args.smack else "OFF"
+    smack_enable_no_mem_split = "ON" if args.mem_no_split else "OFF"
     return f'cmake -DSEA_LINK=llvm-link-10 -DCMAKE_C_COMPILER=clang-10\
     -DCMAKE_CXX_COMPILER=clang++-10 -DSEA_ENABLE_KLEE={use_klee}\
+    -DSEA_ENABLE_SMACK={use_smack} -DSMACK_ENCODING={args.precise} -DSMACK_PROPERTY_CHECK={args.checks}\
+    -DSMACK_ENABLE_NO_MEM_SPLIT={smack_enable_no_mem_split}\
     -DSEA_WITH_BLEEDING_EDGE={use_bleeding_edge} -DSEAHORN_ROOT={SEAHORN_ROOT} ../ -GNinja'
 
 
@@ -84,8 +90,7 @@ def run_ctest_for_seahorn():
         verify_flags = " ".join(extra)
         print(f'Run SeaHorn with extra configs: {verify_flags} ')
         set_env = f'env VERIFY_FLAGS=\"{verify_flags}\"'
-    cmake_conf = make_new_cmake_conf(
-        "OFF", "ON" if args.bleed_edge else "OFF")
+    cmake_conf = make_new_cmake_conf()
     command_lst = ["rm -rf *", cmake_conf, "ninja",
         f'{set_env} ctest -j{os.cpu_count()} -D ExperimentalTest -R . --timeout {args.timeout}']
     cddir = "cd " + BUILDABSPATH
@@ -115,8 +120,7 @@ def collect_stat_from_ctest_log(outfile):
 
 
 def run_ctest_for_klee():
-    use_klee = "ON"
-    cmake_conf = make_new_cmake_conf("ON", "ON" if args.bleed_edge else "OFF")
+    cmake_conf = make_new_cmake_conf()
     command_lst = ["rm -rf *", cmake_conf, "ninja",
                    f'ctest -D ExperimentalTest -R klee_ --timeout 2000']
     print("Start making KLEE results...")
@@ -132,6 +136,23 @@ def run_ctest_for_klee():
     _ = process.communicate(cddir.encode())
     collect_res_from_ctest(FILE_DICT["klee"])
 
+def run_ctest_for_smack():
+    cmake_conf = make_new_cmake_conf()
+    command_lst = ["rm -rf *", cmake_conf, "ninja",
+                   f'ctest -j{os.cpu_count()} -D ExperimentalTest -R smack_ --timeout 200']
+    print("Start making SMACK results...")
+    cddir = "cd " + BUILDABSPATH
+    for strcmd in command_lst:
+        cddir += " ; " + strcmd
+    if args.debug:
+        print(cddir)
+    process = subprocess.Popen(
+        '/bin/bash',
+        stdin=subprocess.PIPE,
+        stdout=get_output_level())
+    _ = process.communicate(cddir.encode())
+    mem_split = 'mem_no_split' if args.mem_no_split else 'mem_split'
+    collect_res_from_ctest(extra_to_filename([str(args.precise), str(args.checks), mem_split]))
 
 def main():
     os.makedirs(DATAABSPATH, exist_ok=True)
@@ -140,6 +161,8 @@ def main():
         run_ctest_for_seahorn()
     if args.klee:
         run_ctest_for_klee()
+    if args.smack:
+        run_ctest_for_smack()
 
 
 if __name__ == "__main__":
@@ -152,7 +175,18 @@ if __name__ == "__main__":
     parser.add_argument('--debug', action='store_true', default=False)
     parser.add_argument('--timeout', type=int, default=2000,
         help='Seconds before timeout for each test')
+    subparsers = parser.add_subparsers(help='sub-commands help')
+    smack_parser = subparsers.add_parser('smack', help="smack help")
+    smack_parser.add_argument('--smack', action='store_true', default=False)
+    smack_parser.add_argument('--precise', type=str, default='unbounded-integer',
+        help='Set Smack precise: unbounded-integer or bit vector')
+    smack_parser.add_argument('--checks', type=str, default='assertions',
+        help='Set Smack property checks: assertions or valid-deref')
+    smack_parser.add_argument('--mem_no_split', action='store_true', default=False,
+        help='Set Smack uses no memory split')
     args, extra = parser.parse_known_args()
     if args.klee:
+        args.seahorn = False
+    if args.smack:
         args.seahorn = False
     main()
