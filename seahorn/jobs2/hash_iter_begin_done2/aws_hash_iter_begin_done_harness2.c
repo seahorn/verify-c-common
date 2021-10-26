ebe6d0f9f8dc14e99756c17f07c0eaa6768853d9
Alex Chen <ac3chen@uwaterloo.ca>
2021-10-26 02:42:31 +0000

    feat(shadow mem): introduce a is_modified version of hash jobs
    Note that hash_table_eq2 has not been committed, pending investigation

---
+++ seahorn/jobs2/hash_iter_begin_done2/aws_hash_iter_begin_done_harness2.c
@@ -0,0 +1,37 @@
+/**
+ */
+
+#include <aws/common/hash_table.h>
+#include <aws/common/private/hash_table_impl.h>
+
+#include <seahorn/seahorn.h>
+
+#include <config.h>
+#include <hash_table_helper.h>
+#include <proof_allocators.h>
+#include <utils.h>
+
+int main(void) {
+  struct aws_hash_table map;
+
+  initialize_bounded_aws_hash_table(&map, MAX_TABLE_SIZE);
+  assume(aws_hash_table_is_valid(&map));
+  assume((map.p_impl->entry_count == 0) == aws_hash_table_deep_is_empty(&map));
+  struct store_byte_from_buffer old_byte;
+  save_byte_from_hash_table(&map, &old_byte);
+  struct aws_hash_iter iter = aws_hash_iter_begin(&map);
+
+  sassert(aws_hash_iter_is_valid(&iter));
+  sassert(iter.status == AWS_HASH_ITER_STATUS_DONE ||
+          iter.status == AWS_HASH_ITER_STATUS_READY_FOR_USE);
+    /* quote header file comment:
+    "...This function cannot fail, but if there are no elements in the table,
+    the returned iterator will return true for aws_hash_iter_done(&iter)."
+    */
+  if (map.p_impl->entry_count == 0) {
+    sassert(aws_hash_iter_done(&iter));
+  }
+  sassert(aws_hash_table_is_valid(&map));
+  assert_hash_table_unchanged(&map, &old_byte);
+  return 0;
+}
---
+++ seahorn/jobs2/hash_iter_done2/aws_hash_iter_done_harness2.c
@@ -0,0 +1,40 @@
+/**
+ */
+
+#include <aws/common/hash_table.h>
+#include <aws/common/private/hash_table_impl.h>
+
+#include <seahorn/seahorn.h>
+
+#include <config.h>
+#include <hash_table_helper.h>
+#include <proof_allocators.h>
+#include <utils.h>
+
+int main(void) {
+  struct aws_hash_table map;
+  initialize_bounded_aws_hash_table(&map, MAX_TABLE_SIZE);
+  assume(aws_hash_table_is_valid(&map));
+
+  struct aws_hash_iter iter;
+  mk_valid_aws_hash_iter(&iter, &map);
+  #ifdef __KLEE__
+  if (iter.status == AWS_HASH_ITER_STATUS_DELETE_CALLED)
+       return 0;
+  #else
+  assume(iter.status == AWS_HASH_ITER_STATUS_DONE ||
+         iter.status == AWS_HASH_ITER_STATUS_READY_FOR_USE);
+  #endif
+  enum aws_hash_iter_status old_status = iter.status;
+  struct store_byte_from_buffer old_byte;
+  save_byte_from_hash_table(&map, &old_byte);
+
+  bool rval = aws_hash_iter_done(&iter);
+
+  sassert(aws_hash_iter_is_valid(&iter));
+  sassert(rval == (iter.status == AWS_HASH_ITER_STATUS_DONE));
+  sassert(iter.status == old_status);
+  sassert(aws_hash_table_is_valid(&map));
+  assert_hash_table_unchanged(&map, &old_byte);
+  return 0;
+}
