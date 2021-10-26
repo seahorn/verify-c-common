0185bc024720a3fef281ea0d0dfa22e0f4cad0a4
Alex Chen <ac3chen@uwaterloo.ca>
2021-10-26 02:42:31 +0000

    feat(shadow mem): introduce a is_modified version of hash jobs
    Note that hash_table_eq2 has not been committed, pending investigation

---
+++ seahorn/jobs2/hash_table_get_entry_count2/aws_hash_table_get_entry_count_harness2.c
@@ -0,0 +1,35 @@
+#include <aws/common/hash_table.h>
+#include <aws/common/math.h>
+#include <aws/common/private/hash_table_impl.h>
+
+#include <seahorn/seahorn.h>
+
+#include <hash_table_helper.h>
+#include <proof_allocators.h>
+#include <utils.h>
+
+int main(void) {
+  struct aws_hash_table table;
+#if defined(__KLEE__) || defined(__FUZZ__)
+  initialize_bounded_aws_hash_table(&table, MAX_TABLE_SIZE);
+#else
+  /* There are no loops in the code under test, so use the biggest possible
+   * value */
+  initialize_bounded_aws_hash_table(&table, SIZE_MAX);
+#endif
+  assume(aws_hash_table_is_valid(&table));
+  struct hash_table_state *state = table.p_impl;
+
+  struct store_byte_from_buffer stored_byte;
+  save_byte_from_hash_table(&table, &stored_byte);
+  size_t old_entry_count = state->entry_count;
+  size_t rval = aws_hash_table_get_entry_count(&table);
+  sassert(rval == old_entry_count);
+  /* Ensure that the table remains valid */
+  sassert(aws_hash_table_is_valid(&table));
+
+  /* Ensure that table is unchanged */
+  assert_hash_table_unchanged(&table, &stored_byte);
+  return 0;
+}
+
