f9d060f224eea075c4e01e2fdbd4147dae4d8716
Alex Chen <ac3chen@uwaterloo.ca>
2021-12-11 22:56:55 +0000

    After change

--- seahorn/jobs2/hash_table_eq2/aws_hash_table_eq_harness2.c
+++ seahorn/jobs2/hash_table_eq2/aws_hash_table_eq_harness2.c
@@ -1,30 +1,36 @@
 #include <aws/common/hash_table.h>
 #include <aws/common/private/hash_table_impl.h>
-#include <config.h>
+#include <config.h> 
 #include <hash_table_helper.h>
 #include <seahorn/seahorn.h>
-#include <utils.h>
-
+#include <utils.h> 
 int main(void) {
+  /* parameters */
   struct aws_hash_table map_a;
   initialize_bounded_aws_hash_table(&map_a, MAX_TABLE_SIZE);
   map_a.p_impl->equals_fn = nondet_equals;
-  assume(aws_hash_table_is_valid(&map_a));
-  struct store_byte_from_buffer old_byte_a;
-  save_byte_from_hash_table(&map_a, &old_byte_a);
-
   struct aws_hash_table map_b;
   initialize_bounded_aws_hash_table(&map_b, MAX_TABLE_SIZE);
   map_b.p_impl->equals_fn = nondet_equals;
-  assume(aws_hash_table_is_valid(&map_b));
-  struct store_byte_from_buffer old_byte_b;
-  save_byte_from_hash_table(&map_b, &old_byte_b);
 
+  /* assumptions */
+  assume(aws_hash_table_is_valid(&map_a));
+  assume(aws_hash_table_is_valid(&map_b));
+
+  /* save current state */
+  sea_tracking_on();
+  sea_reset_modified((char *)map_a.p_impl);
+  sea_reset_modified((char *)map_b.p_impl);
+
+  /* operation under verification */
   bool rval = aws_hash_table_eq(&map_a, &map_b, nondet_equals);
 
+  /* assertions */
   sassert(aws_hash_table_is_valid(&map_a));
   sassert(aws_hash_table_is_valid(&map_b));
-  assert_hash_table_unchanged(&map_a, &old_byte_a);
-  assert_hash_table_unchanged(&map_b, &old_byte_b);
+
+  sassert(!sea_is_modified((char *)map_a.p_impl));
+  sassert(!sea_is_modified((char *)map_b.p_impl));
+
   return 0;
 }
