0c6aabc58090bf1dbf4a761e69bc920c098d2283
Yusen Su <y256su@uwaterloo.ca>
2022-01-10 15:27:21 -0500

    fix(lib): add implementation of malloc and memset for symbiotic

--- seahorn/lib/klee_allocators.c
+++ seahorn/lib/klee_allocators.c
@@ -3,11 +3,21 @@
 #include <klee_switch.h>
 #include <nondet.h>
 
+#ifdef __SYMBIOTIC__
+extern void klee_make_symbolic(void *, size_t, const char *);
+extern void *__VERIFIER_malloc0(size_t size);
+void *symb_malloc(size_t sz) {
+  void *mem = __VERIFIER_malloc0(sz);
+  klee_make_symbolic(mem, sz, "malloc0");
+  return mem;
+}
+#endif
+
 // Note this behaviour deviates from SEAHORN since this can fail to allocate
 // memory.
 void *bounded_malloc_havoc(size_t size) {
   #ifdef __SYMBIOTIC__
-    void* data = size == 0 ? NULL : malloc(size);
+    void* data = size == 0 ? NULL : symb_malloc(size);
   #else
     void* data = size == 0 ? NULL : malloc(alloc_size(size));
   #endif
