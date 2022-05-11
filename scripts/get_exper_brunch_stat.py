a9517f85a64afbb92c51a9606064db5646ef7923
Xiang Zhou <x245zhou@uwaterloo.ca>
2022-05-11 14:52:09 -0400

    feat(script): adjust metrics to collect with yaml file

--- scripts/get_exper_brunch_stat.py
+++ scripts/get_exper_brunch_stat.py
@@ -2,28 +2,36 @@ import xml.etree.ElementTree as ET
 import re
 import csv
 import argparse
+import yaml
 import glob
 from collections import defaultdict
 
+METRICS_FILE = "metrics.yaml"
 
 def read_brunchstat_from_log(log_file_name):
+    with open(METRICS_FILE, 'r') as mtyml:
+        metrics = yaml.safe_load(mtyml)
     log_file = open(log_file_name, 'r')
     line = log_file.readline()
-    data = defaultdict(list)
+    data = list() # list of metric_name->metric
     cur_test = None
     while line:
         # look for next test
         new_test = re.search("Test: ", line)
         if new_test:
+            new_test_data = defaultdict(lambda: 'n/a')
             span = new_test.span()
             test_name = line[span[1]:]
             # remove _unsat_test
             cur_test = test_name[:-12]
+            new_test_data['job_name'] = cur_test
+            data.append(new_test_data)
         elif line.startswith("BRUNCH_STAT"):
             stat = line.split()
-            # stat_name = " ".join(stat[1:-1])
-            stat_num = stat[-1]
-            data[cur_test].append(stat_num)
+            stat_name = " ".join(stat[1:-1])
+            if stat_name in metrics:
+                stat_num = stat[-1]
+                data[-1][stat_name] = stat_num
         line = log_file.readline()
 
     log_file.close()
@@ -98,22 +106,15 @@ def write_symbiotic_bruchstat_into_csv(data, out_file):
 
 
 def write_brunchstat_into_csv(data, out_file):
-    with open(out_file, 'w+', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([
-            "job_name",
-            "result",
-            "bmc.circ_sz",
-            "bmc.dag_sz",
-            "bmc_time",
-            "bmc_solve_time",
-            "cda_time", "gssa_time",
-            "opsem_assert_time",
-            "opsem_simp_time",
-            "seahorn_total_time"])
-        for k, v in data.items():
-            writer.writerow([k, *v])
-
+  with open(METRICS_FILE, 'r') as mtyml:
+    metrics = yaml.safe_load(mtyml)
+    metric_serlz = [(k, v) for k, v in metrics.items()] # ensure ordering
+  with open(out_file, 'w+', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow((v for _, v in metric_serlz))
+    for job_data in data:
+      row = (job_data[k] for k, _ in metric_serlz)
+      writer.writerow(row)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
